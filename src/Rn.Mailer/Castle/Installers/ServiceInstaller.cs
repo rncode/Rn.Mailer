231f181990a154995a0ee37bb236f7c0eef3fcd7
Richard Niemand <niemand.richard@gmail.com>
2016-10-10 22:32:52 +0200

    Updates

---
+++ src/Rn.Mailer/Castle/Installers/ServiceInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Rn.Core.Config;
+
+namespace Rn.Mailer.Castle.Installers
+{
+    public class ServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component
+                .For<IWebConfig>()
+                .ImplementedBy<RnWebConfig>()
+                .LifestyleSingleton());
+        }
+    }
+}
