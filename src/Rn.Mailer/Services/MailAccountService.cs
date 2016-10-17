21b98702557bb71156df72dcf9d33e6bd69fdbd6
Richard Niemand <niemand.richard@gmail.com>
2016-10-17 20:28:55 +0200

    Added ability for MailAccountService to decrypt mail account passwords

--- src/Rn.Mailer/Services/MailAccountService.cs
+++ src/Rn.Mailer/Services/MailAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Rn.Core.Encryption;
 using Rn.Mailer.Core.Models;
 using Rn.Mailer.DAL.Repos;
 
@@ -12,15 +13,22 @@ namespace Rn.Mailer.Services
     public class MailAccountService : IMailAccountService
     {
         private readonly IMailAccountsRepo _accountsRepo;
+        private readonly IEncryptionService _encryptionService;
 
-        public MailAccountService(IMailAccountsRepo accountsRepo)
+        public MailAccountService(IMailAccountsRepo accountsRepo, IEncryptionService encryptionService)
         {
             _accountsRepo = accountsRepo;
+            _encryptionService = encryptionService;
         }
 
         public async Task<MailAccount> GetMailAcount(int accountId)
         {
-            return await _accountsRepo.GetAccount(accountId);
+            var account = await _accountsRepo.GetAccount(accountId);
+
+            // Decrypt the accounts password
+            account.Password = _encryptionService.DecryptText(account.Password);
+
+            return account;
         }
     }
 }
