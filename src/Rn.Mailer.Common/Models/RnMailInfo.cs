5950b4169ac583a8520aa241f8f187df78dedc06
Richard Niemand <niemand.richard@gmail.com>
2016-10-26 16:25:28 +0200

    Check in

---
+++ src/Rn.Mailer.Common/Models/RnMailInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Rn.Mailer.Common.Models
+{
+    public class RnMailInfo
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Body { get; set; }
+        public string Subject { get; set; }
+    }
+}
--- src/Rn.Mailer.Core/MailClient.cs
+++ src/Rn.Mailer.Core/MailClient.cs
@@ -1,34 +1,52 @@
-﻿using System.Net.Mail;
+﻿using System.Net;
+using System.Net.Mail;
 using Rn.Mailer.Core.Models;
 
 namespace Rn.Mailer.Core
 {
     public interface IMailClient
     {
-
+        int MailAccountId { get; }
+        int OwningUserId { get; }
     }
 
     public class MailClient : IMailClient
     {
+        public int MailAccountId { get; }
+        public int OwningUserId { get; }
+
         private readonly MailAccount _mailAccount;
         private readonly SmtpClient _smtpClient;
 
         public MailClient(MailAccount mailAccount)
         {
+            // Set basic information about our mail account
             _mailAccount = mailAccount;
+            OwningUserId = mailAccount.UserId;
+            MailAccountId = mailAccount.Id;
 
-
-
-
-
+            // Create our base SmtpClient
             _smtpClient = new SmtpClient
             {
-                UseDefaultCredentials = MustUseDefaultCredentials(mailAccount)
+                UseDefaultCredentials = MustUseDefaultCredentials(mailAccount),
+                Host = mailAccount.Host,
+                Port = mailAccount.Port,
+                EnableSsl = mailAccount.UseSsl
             };
+
+            // Decide if we need to set credentials for the clients user
+            if (_smtpClient.UseDefaultCredentials == false)
+            {
+                _smtpClient.Credentials = new NetworkCredential
+                {
+                    UserName = mailAccount.Username,
+                    Password = mailAccount.Password
+                };
+            }
         }
 
         // Helper methods
-        private bool MustUseDefaultCredentials(MailAccount mailAccount)
+        public static bool MustUseDefaultCredentials(MailAccount mailAccount)
         {
             if (!string.IsNullOrWhiteSpace(mailAccount.Username))
                 return false;
